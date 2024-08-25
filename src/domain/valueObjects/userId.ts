0c6dea6725491411855c7174b4d858cb170c103a
Madnoberson <baseddepartmentzx77@gmail.com>
2024-08-25 15:43:05 +0400

    Refactor `UserId` value object

--- src/domain/valueObjects/userId.ts
+++ src/domain/valueObjects/userId.ts
@@ -2,13 +2,11 @@ import { validate } from "uuid";
 
 
 export class UserId {
-    constructor(public readonly value: string) {}
-
-    static new(value: string) {
+    constructor(public readonly value: string) {
         const isUUID = validate(value);
         if (!isUUID) {
             throw Error(`${value} is not valid UUID`);
         }
-        return new UserId(value);
+        this.value = value
     }
 }
