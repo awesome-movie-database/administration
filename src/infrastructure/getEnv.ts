e75186bd991e4c6cffaf2d14e847d399d03ae0f6
Madnoberson <baseddepartmentzx77@gmail.com>
2024-08-31 09:54:28 +0400

    Add docstring for `envVarByKey`

--- src/infrastructure/getEnv.ts
+++ src/infrastructure/getEnv.ts
@@ -1,6 +1,10 @@
 import { env } from "process";
 
 
+/**
+* Returns value from env vars by key
+* @throws {Error} If value doesn't exist.
+*/
 export function envVarByKey(key: string): string {
     const value = env[key]
     if (value === undefined) {
